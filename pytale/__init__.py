632c45c7738a14bd8dabb132a778e5a9d4923d82
toripppppy <Lundy11@icloud.com>
2023-03-24 22:47:44 +0900

    ライブラリ公開用のファイル構成を作成

---
+++ pytale/__init__.py
@@ -0,0 +1,1 @@
+from pytale.pytale import *
---
+++ pytale/pytale.py
@@ -0,0 +1,8 @@
+class Scenario:
+
+    def __init__(self, filepath: str) -> None:
+        '''
+        filepath: "path/to/xxxxx.json"
+        '''
+    
+    

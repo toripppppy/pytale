1d4f6328e337631a3df0a3285f1f961161dd98c0
toripppppy <Lundy11@icloud.com>
2023-03-25 00:30:23 +0900

    jsonの読み書きを実装

--- pytale/pytale.py
+++ pytale/pytale.py
@@ -1,4 +1,6 @@
 import os
+import json
+from collections import OrderedDict
 
 class Scenario:
 
@@ -17,5 +19,18 @@ class Scenario:
         self.filepath = filepath
     
 
-    def get_filepath(self):
+    def get_filepath(self) -> None:
         return self.filepath
+    
+    
+    # jsonを取得
+    def get_json(self) -> object:
+        with open(self.filepath) as f:
+            # 要素の順番を崩さずに読み込み
+            di = json.load(f, object_pairs_hook=OrderedDict)
+            return di
+        
+    # jsonを更新
+    def dump_json(self, di) -> None:
+        with open(self.filepath, 'w') as f:
+            json.dump(di, f, indent=4, ensure_ascii=False)
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import pytale
 
 s = pytale.Scenario('tests/scenario.json')
-print(s.get_filepath())
+print(s.get_json())
+s.dump_json({"a":['a','b','c']})
